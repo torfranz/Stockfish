267acf5ed083a15c5dba304a094868e268f06541
torfranz <tfranzer@gmx.de>
2017-10-31 19:34:32 +0100

    bench 5321587 - use half bonus

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -214,7 +214,7 @@ namespace {
   const Score MinorBehindPawn     = S( 16,  0);
   const Score BishopPawns         = S(  8, 12);
   const Score LongRangedBishop    = S( 22,  0);
-  const Score RookOutpost         = S( 20,  0);
+  const Score RookOutpost         = S( 10,  0);
   const Score RookOnPawn          = S(  8, 24);
   const Score TrappedRook         = S( 92,  0);
   const Score WeakQueen           = S( 50, 10);
