ade34438a0d1ac5eba237c408dc3182609915140
Torsten Franz <torsten_franz@agilent.com>
2018-01-11 15:58:19 +0100

    5111384 extend to 5th row

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -350,7 +350,7 @@ namespace {
             }
 
             // Bonus when behind a pawn
-            if (    relative_rank(Us, s) < RANK_5
+            if (    relative_rank(Us, s) < RANK_6
                 && (pos.pieces(PAWN) & (s + pawn_push(Us))))
                 score += MinorBehindPawn;
 
