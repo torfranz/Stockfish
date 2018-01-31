78a57c70d1d8730df8a164d6cb528a34d5094f2e
Torsten Franz <torsten_franz@agilent.com>
2018-01-31 10:53:28 +0100

    updae

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -294,6 +294,7 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
 
   Value bonus = shelter_storm<Us>(pos, ksq);
 
+  // ELO?5298149
   // If we can castle use the bonus after the castling if it is bigger
   if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
       bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)));
