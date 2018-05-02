9a3a9bb42d92116e77246bab3b583501babdcf51
Torsten Franz <torsten_franz@agilent.com>
2018-05-02 15:28:58 +0200

    bench 4922502 - only consider center

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -355,7 +355,7 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
-                                     * (1 + popcount(blocked & CenterFiles));
+                                     * (1 + popcount(blocked & Center));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(Center & (attacks_bb<BISHOP>(s, pos.pieces(PAWN)) | s)))
