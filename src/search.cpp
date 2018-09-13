a49021f0daed9d028b991cf081e9b06435069fca
Torsten Franz <tfranzer@gmx.de>
2018-09-13 22:19:26 +0200

    bench 4324625

--- src/search.cpp
+++ src/search.cpp
@@ -965,7 +965,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Prune moves with negative SEE (~10 Elo)
-              if (!pos.see_ge(move, Value(-29 * lmrDepth * lmrDepth)))
+              if (!pos.see_ge(move, Value(-lmrDepth * lmrDepth * lmrDepth * lmrDepth)))
                   continue;
           }
           else if (   !extension // (~20 Elo)
