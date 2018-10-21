72aa127b08d0eb5b1e6c1ecee616b33844b74cae
Torsten Franz <tfranzer@gmx.de>
2018-10-21 22:55:33 +0200

    bench 4577396 - take 5 q-agility (2, 0)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -371,9 +371,6 @@ namespace {
 
         if (Pt == ROOK)
         {
-            // give bonus/malus for number of direction the queen can move
-            score += make_score(2, 0) * (popcount(RookAgilityDirections[s] & b & mobilityArea[Us]) - 2);
-
             // Bonus for aligning rook with enemy pawns on the same rank/file
             if (relative_rank(Us, s) >= RANK_5)
                 score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
