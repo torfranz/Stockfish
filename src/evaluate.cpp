f3236ff403ebbe4f2303fdb34eb9063a7ecbdc72
Torsten Franz <tfranzer@gmx.de>
2019-08-02 18:31:08 +0200

    bench 4176122

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -759,7 +759,7 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 16 + 4 * pe->passed_count();
         else
-            sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * (pos.count<PAWN>(strongSide) - pe->blocked_count(strongSide)), sf);
+            sf = std::min(40 + (pos.opposite_bishops() ? 3 : 10) * (pos.count<PAWN>(strongSide) - pe->blocked_count(strongSide)), sf);
 
     }
 
